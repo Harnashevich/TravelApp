9557d061d0645727a150522f39b221a0e1537fdb
Andrei <garnash1313@gmail.com>
2024-08-30 14:01:39 +0300

    Initial Commit

---
+++ TravelApp/TravelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAppApp.swift
+//  TravelApp
+//
+//  Created by Andrei Harnashevich on 30.08.24.
+//
+
+import SwiftUI
+
+@main
+struct TravelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
